1211c62d4f766105a028d756776b94c00e005fb4
Hemavati29 <hemavatitanpure@gmail.com>
2021-05-12 13:08:57 +0530

    Array Manipulation

--- program.js
+++ program.js
@@ -1,4 +1,33 @@
 // Hello
 
-// Input array=["ss","ssss"]
+// const array=["dog","cat","cow","cat","dog","dog","cow"]
 
+// output obj={"dog":2,"cat":1}
+
+
+
+const Array= ["dog","cat","cow","cat","dog","dog","cow"]
+
+
+function Count(Array){
+    var variable1={};
+
+    for(var i=0;i<Array.length;i++)
+    {
+
+        if(variable1.hasOwnProperty(Array[i]))
+        {
+          
+            variable1[Array[i]]=variable1[Array[i]]+1     //for counting
+        }
+
+        else
+        {
+            variable1[Array[i]]=1
+        }
+    }
+
+    return variable1
+}
+
+console.log(Count(Array));
